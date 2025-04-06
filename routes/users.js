92e0cb69f2f6607671595455df7b686810a04de7
DigiDias <sammy.gouljiar@gmail.com>
2025-04-06 20:31:56 +0400

    generation du projet express

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const express = require('express');
+const router = express.Router();
+
+const service = require('../services/users');
+const private = require('../middlewares/private');
+
+router.get('/:id', private.checkJWT, service.getUserById);  // Lire un utilisateur
+router.put('/add', service.add);  //  Ajouter un utilisateur
+router.patch('/:id', private.checkJWT, service.update);  //  Modifier un utilisateur
+router.delete('/:id', private.checkJWT, service.delete);  //  Supprimer un utilisateur
+router.post('/authenticate', service.authenticate);//Ajout de la route Authenticate
+
+router.get('/', service.getAllUsers);  // Récupérer tous les utilisateurs
+
 
 module.exports = router;
---
+++ services/users.js
@@ -0,0 +1,100 @@
+const User = require('../models/users');
+const bcrypt = require('bcrypt');   
+const jwt = require('jsonwebtoken');
+
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'Utilisateur non trouvé' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}  ;
+
+exports.add = async (req, res) => {
+    const { username, password, email } = req.body;
+    try {
+        const user = new User({ username, password, email });
+        await user.save();
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.update = async (req, res) => {
+    const { username, password, email } = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, { username, password, email }, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: 'Utilisateur non trouvé' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.delete = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'Utilisateur non trouvé' });
+        }
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.authenticate = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    try {
+      // Recherche de l'utilisateur par email
+      let user = await User.findOne({ email: email }, "-__v -createdAt -updatedAt");
+  
+      // Vérifier si l'utilisateur existe
+      if (!user) {
+        return res.status(404).json({ message: "user_not_found" });
+      }
+  
+      // Comparer le mot de passe
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(403).json({ message: "wrong_credentials" });
+      }
+  
+      // Supprimer le mot de passe de l'objet utilisateur avant de l'envoyer
+      delete user._doc.password;
+  
+      // Créer un token JWT
+      const expiresIn = 24 * 60 * 60; // Durée de validité du token (24 heures)
+      const token = jwt.sign({ user: user }, SECRET_KEY, { expiresIn });
+  
+      // Ajouter le token dans l'en-tête Authorization
+      res.header("Authorization", "Bearer " + token);
+  
+      // Réponse avec le token JWT
+      return res.status(200).json({ message: "authenticate_succeed", token: token });
+  
+    } catch (error) {
+      console.error("Error authenticating user:", error);  // Ajout de log détaillé
+      return res.status(500).json({ message: "Error authenticating user", error: error.message });
+    }
+  };
+  
+  
+  
+  exports.getAllUsers = async (req, res) => {
+      try {
+          const users = await User.find(); // Récupère tous les utilisateurs
+          res.json(users);
+      } catch (error) {
+          res.status(500).json({ error: 'Internal Server Error' });
+      }
+  };
+  
