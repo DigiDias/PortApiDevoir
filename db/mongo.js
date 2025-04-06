e54e65dac7a886bc8cecca451e41c9e0d7633601
DigiDias <sammy.gouljiar@gmail.com>
2025-04-06 20:02:09 +0400

    ajout du models Users

--- db/mongo.js
+++ db/mongo.js
@@ -1,9 +1,41 @@
+/**
+ * Module de connexion à MongoDB.
+ * 
+ * Ce module initialise la connexion à la base de données MongoDB en utilisant Mongoose.
+ * Il utilise l'URL de la base de données définie dans les variables d'environnement.
+ * 
+ * @module mongo
+ */
+
 const mongoose = require('mongoose');
 
+// Options pour la connexion à la base de données MongoDB
+/**
+ * @typedef {Object} ClientOptions
+ * @property {string} dbName - Le nom de la base de données à utiliser (ici 'PortApiDevoir').
+ */
 const clientOptions = {
   dbName: 'PortApiDevoir',
 };
 
+/**
+ * Initialise la connexion à MongoDB en utilisant Mongoose.
+ * 
+ * Cette fonction établit une connexion à la base de données MongoDB spécifiée par l'URL de connexion
+ * dans les variables d'environnement, et configure les options de connexion.
+ * 
+ * @async
+ * @function
+ * @throws {Error} Si la connexion échoue, une erreur sera lancée.
+ * 
+ * @example
+ * // Exemple d'appel de la fonction pour initier la connexion
+ * initClientDbConnection().then(() => {
+ *   console.log('Base de données MongoDB connectée');
+ * }).catch((error) => {
+ *   console.error('Échec de la connexion à MongoDB', error);
+ * });
+ */
 module.exports.initClientDbConnection = async () => {  
     try {
         await mongoose.connect(process.env.URL_MONGO, clientOptions);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Bienvenue sur mon API Port de Plaisance 🎉');
+  res.send('Bienvenue sur mon API Port de Plaisance Russel ');
 });
 module.exports = router;
