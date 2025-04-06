bbd24573b3de0617b963f800ed7f4cf8ccc3ea6c
DigiDias <sammy.gouljiar@gmail.com>
2025-04-06 18:30:36 +0400

    mise en place du projet avec express générator, création de la base de données Mongo

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send('Bienvenue sur mon API Port de Plaisance 🎉');
+});
+module.exports = router;
